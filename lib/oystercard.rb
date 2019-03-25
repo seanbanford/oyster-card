ffbc5ac89795356b41455ee4b5016dc66e77a55b
Soleyman Shahir <Soleymanshahir@yahoo.com>
2019-03-25 16:50:09 +0000

    saving the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,14 @@
 
 class Oystercard
 
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :in_journey, :entry_station
   DEFAULT_MAXIMUM_FUNDS = 90
   DEFAULT_MINIMUM_FUNDS = 1
 
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(value)
@@ -15,19 +16,24 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    if @entry_station == nil
+      false
+    else
+      true
+    end
   end
 
-  def touch_in
+  def touch_in(station)
     fail 'Minimum fare £1' if minimum?
     @in_journey = true
+    @entry_station = station
   end
 
   def touch_out(fare = DEFAULT_MINIMUM_FUNDS)
     deduct(fare)
     @in_journey = false
+    @entry_station = nil
   end
-
 
 private
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station){ double :station }
   describe '#card balance' do
     it 'responds to balance being 0' do
       expect(subject.balance).to eq 0
@@ -22,13 +23,13 @@ describe Oystercard do
     end
 
     it 'Has minimum fare available' do
-      expect { subject.touch_in }.to raise_error 'Minimum fare £1'
+      expect { subject.touch_in(station) }.to raise_error 'Minimum fare £1'
     end
   end
 
   it 'touches in' do
     subject.top_up(2)
-    subject.touch_in
+    subject.touch_in(station)
     expect(subject.in_journey?).to eq true
   end
 
@@ -45,4 +46,17 @@ describe Oystercard do
     expect(subject.in_journey?).to eq false
   end
 
+
+  it 'remembers entry station' do
+    subject.top_up(2)
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq station
+  end
+
+  it 'forgets entry station on touch out' do
+    subject.top_up(2)
+    subject.touch_in(station)
+    subject.touch_out
+    expect(subject.entry_station).to eq nil
+  end
 end
