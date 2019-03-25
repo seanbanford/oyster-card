1185dbe13a3094d62aac2b65f5bcd83da1cdf865
Soleyman Shahir <Soleymanshahir@yahoo.com>
2019-03-25 14:56:53 +0000

    added max value and deduct method and test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,7 +5,20 @@ class Oystercard
 
   def initialize
     @balance = 0
-  end 
+  end
 
+  def top_up(value)
+    fail 'Maximum £90' if max?(value)
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+private
+
+  def max?(value)
+    (@balance += value) > 90
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,20 @@ describe Oystercard do
   it 'responds to balance being 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'allows balance to top up' do
+    subject.top_up(5)
+    expect(subject.balance).to eq 5
+  end
+
+  it 'balance maximum of £90' do
+    subject.top_up(90)
+    expect { subject.top_up(1) }.to raise_error 'Maximum £90'
+  end
+
+  it 'can deduct a fare' do
+    subject.deduct(10)
+    expect(subject.balance).to eq -10
+  end
+
 end
